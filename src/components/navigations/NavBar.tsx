8f93f3b3b56a718d95d1b7a193910b4e090c271c
Den <dimass.drm@gmail.com>
2025-01-15 18:26:13 +0700

    CHANGE: nav layout

--- src/components/navigations/NavBar.tsx
+++ src/components/navigations/NavBar.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router";
 
 import { navLinks } from "../../constants";
 
+type ThemeList = "dark" | "light" | "os";
+
 export default function NavBar() {
+  const [themeState, setThemeState] = useState<ThemeList>((localStorage.getItem("theme") as ThemeList) || "os");
+
+  function switchTheme(theme: ThemeList) {
+    if (theme == "os") {
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", (e) => (e.matches ? setThemeState("dark") : setThemeState("light")));
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem("theme", themeState);
+  }, [themeState]);
+
   return (
     <nav
       id="navigation-bar"
@@ -12,10 +29,7 @@ export default function NavBar() {
         <h1 className="font-black text-3xl tracking-tighter">Aksa Media</h1>
       </NavLink>
 
-      <div className="">
-        {/* Dropdown goes here */}
-        <div id=""></div>
-
+      <div className="flex gap-4">
         {/* Main Routes */}
         <ul id="nav-list-parent" className="flex items-center gap-2">
           {navLinks.map((p, i) => (
@@ -25,8 +39,7 @@ export default function NavBar() {
                 className={({
                   isActive,
                   isTransitioning,
-                }) => `text-xl px-2 pb-0.5 tracking-tighter text-center ease-in-out duration-300 relative font-medium hover:text-black z-0
-                before:absolute before:left-0 before:top-0 before:h-full before:bg-yellow before:w-0 hover:before:w-full before:origin-left  before:ease-in-out before:duration-300 before:-z-10
+                }) => `text-xl px-2 pb-0.5 tracking-tighter text-center ease-in-out duration-300 relative font-medium hover-effect
                 ${isActive ? "before:w-full text-black " : ""}
                 ${isTransitioning ? "before:rounded-lg" : ""}
                 `}
@@ -36,6 +49,13 @@ export default function NavBar() {
             </li>
           ))}
         </ul>
+
+        <div id="theme-switcher" className="">
+          <div className="w-8 h-8 rounded-full bg-black cursor-pointer" onClick={() => switchTheme("dark")} />
+        </div>
+
+        {/* Dropdown goes here */}
+        <div id=""></div>
       </div>
     </nav>
   );
